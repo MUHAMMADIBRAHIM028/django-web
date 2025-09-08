ca30c9e37ae1ffba404fd566803d1a504f1b6006
MUHAMMADIBRAHIM028 <ibrahimkhanalmani@gmail.com>
2025-09-09 03:34:07 +0500

    update code web

--- djangoweb/homepage/views.py
+++ djangoweb/homepage/views.py
@@ -8,7 +8,7 @@ def index(request):
         <head><title>Welcome</title></head>
         <body style="display:flex; justify-content:center; align-items:center; height:100vh; font-size:24px; background:#f4f4f4;">
             <div>
-                <h1>Hello This is my fist web on django.! 🌍</h1>
+                <h1>Hello This is my fist web on django in this month.! 🌍</h1>
                 <p>Current time and date: <b>{now.strftime('%Y-%m-%d %H:%M:%S')}</b></p>
             </div>
         </body>
@@ -16,5 +16,6 @@ def index(request):
     """
     return HttpResponse(html)
 from django.shortcuts import render
+
 
 # Create your views here.
