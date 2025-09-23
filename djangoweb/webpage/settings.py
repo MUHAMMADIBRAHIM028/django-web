dcf2162e85a387c4c9f726fe95c7da59594b8592
MUHAMMADIBRAHIM028 <122287160+MUHAMMADIBRAHIM028@users.noreply.github.com>
2025-09-23 05:39:11 +0500

    Update settings.py

--- djangoweb/webpage/settings.py
+++ djangoweb/webpage/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'myprojectdb',           # Database name you created
         'USER': 'myprojectuser',         # User you created
         'PASSWORD': 'mypassword',        # Password for the user
-         'HOST': '56.228.24.7',        # EC2 public IP or private if same server
+         'HOST': 'localhost',        # EC2 public IP or private if same server
         'PORT': '5432', 
     }
 }
